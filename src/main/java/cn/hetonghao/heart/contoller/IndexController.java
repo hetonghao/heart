494c863e840ef6081cdb73c68cd66b842fdd9cda
hth <hetonghao>
2020-03-22 15:22:42 +0800

    集成Thymelaef

--- src/main/java/cn/hetonghao/heart/contoller/IndexController.java
+++ src/main/java/cn/hetonghao/heart/contoller/IndexController.java
@@ -1,27 +1,25 @@
 package cn.hetonghao.heart.contoller;
 
-import cn.hetonghao.heart.entity.Article;
 import cn.hetonghao.heart.mapper.ArticleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author HeTongHao
  * @since 2020/3/17 23:45
  */
-@RestController
-@RequestMapping("index")
+@Controller
 public class IndexController {
     @Autowired
     private ArticleMapper articleMapper;
 
-    @GetMapping("hello")
-    public String hello() {
-        List<Article> articles = articleMapper.list();
-        return "hello hth";
+    @GetMapping("index")
+    public String hello(Model model) {
+        model.addAttribute("msg", "hello,Thymeleaf!My name is HTH!");
+        model.addAttribute("htmlTag", "<h2>html标签转义</h2");
+        model.addAttribute("articles", articleMapper.list());
+        return "index";
     }
 }
